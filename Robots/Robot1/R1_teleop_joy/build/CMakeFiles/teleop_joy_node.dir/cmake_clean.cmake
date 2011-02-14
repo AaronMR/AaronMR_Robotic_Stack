7b2a738630111d2f48ec8258c1ab4c1ee3a70910
Aaron Martinez Romero <aaronmartinezromero@gmail.com>
2011-02-14 17:43:48 +0100

    new node to teleoperate the robot with joy

---
+++ Robots/Robot1/R1_teleop_joy/build/CMakeFiles/teleop_joy_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/teleop_joy_node.dir/src/teleop_joy_node.o"
+  "../bin/teleop_joy_node.pdb"
+  "../bin/teleop_joy_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/teleop_joy_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
